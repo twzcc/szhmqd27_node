4f97c5496f598c3c91a094660d3a8137273b79be
Git <twzcc>
2019-01-20 10:51:39 +0800

    node项目第一次提交

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//导包
+const express = require('express')
+//创建a'p'p
+const app = express()
+//处理请求
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+//启动
+app.listen(3000, '127.0.0.1', err => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('start ok')
+})
